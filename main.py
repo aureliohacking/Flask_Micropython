bdce65a2a842538b9899d3331fe4d3a92818676f
Aureliohacking <52579743+aureliohacking@users.noreply.github.com>
2021-07-31 17:31:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import urequests, dht
-from machine impor Pin
+from machine import Pin
 
 s = dht.DHT11(Pin(14))
 
@@ -11,6 +11,7 @@ hum = s.humidity()
 temp2 = str(temp)
 hum2 = str(hum)
 dato = "humeda=" + hum2 + "&temperatura=" + temp2
+url = "http://10.10.10.187:5000/envia?"
 headers = {'Content-type':"application/x-www-form-urlencoded"}
 data = dato
 
